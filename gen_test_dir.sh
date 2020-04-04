13031ac66611041cfda117772354b0dbc3b8a91f
kyuta3579 <kohei.fukunaga@qtasrv.net>
2020-04-04 23:08:39 +0900

    [debug]test用のスクリプト追加.

---
+++ gen_test_dir.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p test_dir/first/secound
+touch test_dir/one
+touch test_dir/two
+touch test_dir/first/third
+touch test_dir/first/secound/fourth
+touch test_dir/first/secound/fifth
